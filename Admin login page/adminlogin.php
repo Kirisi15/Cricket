6b0ccb300933a675b6cc2f5ac8220c1ea5e3b5c6
Kirisi15 <kkirisigan@gmail.com>
2024-10-16 23:58:07 +0530

    create login page

--- Admin login page/adminlogin.php
+++ Admin login page/adminlogin.php
@@ -6,6 +6,22 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="">
+        <h2>ADMIN LOGIN</h2>
+        <table>
+            <tr>
+                <td><lable for="username">User Name:</lable></td>
+                <td><input type="text" id="username"><br></td>
+            </tr>
+            <tr>
+                <td><lable for="password">Password:</lable></td>
+                <td><input type="password" id="password"><br></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><button type="submit">Log in</button></td>
+            </tr>
+        </table>
+    </div>
 </body>
 </html>
