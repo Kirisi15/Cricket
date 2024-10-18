c8b7d569fd28830f7934e55961271d6279f4b1c6
Sarvo178 <ovindupathirana554@gmail.com>
2024-10-18 20:22:57 +0530

    h

---
+++ playerinsert.php
@@ -0,0 +1,20 @@
+<?php
+    include 'db.php';
+    
+    if(isset($_POST['submit'])){
+        $playername = $_POST['playerName'];
+        $contactnumber = md5($_POST['contactNumber']);
+        $playerimage = $_POST['playerImage'];
+        $teamname = $_POST['teamName'];
+        $role = $_POST['role'];
+        
+        $sql = "INSERT INTO player (playerName, contactNumber, playerImage, teamName, role) VALUES ('$playername', '$contactnumber', '$playerimage', '$teamname', '$role')";
+
+        if($conn->query($sql)=== true){
+            echo " inserted successfully";
+        }
+        else{
+            echo " error";
+        }
+    }
+?>
