e2ee508887699efb410388ab9f698978e08e11a0
Thivya15 <thivyamahendran@gmail.com>
2024-10-19 10:44:34 +0530

    change

---
+++ playerpage.php
@@ -0,0 +1,20 @@
+<?php
+    include 'dbConnect.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+        <h3> Player Details </h3>
+
+        <?php
+        ?>
+    </form>
+</body>
+</html>
